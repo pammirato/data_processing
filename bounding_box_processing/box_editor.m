2593897bb9415595477507949e9eedb6b3a3feec
pammirato <pammirato10@aol.com>
2016-09-22 15:01:12 -0400

    started cleaning

--- bounding_box_processing/box_editor.m
+++ bounding_box_processing/box_editor.m
@@ -1,28 +1,27 @@
 function varargout = box_editor(varargin)
-% BOX_EDITOR MATLAB code for box_editor.fig
-%      BOX_EDITOR, by itself, creates a new BOX_EDITOR or raises the existing
-%      singleton*.
+% BOX_EDITOR a GUI for editing/removing bounding boxes in images 
 %
-%      H = BOX_EDITOR returns the handle to a new BOX_EDITOR or the handle to
-%      the existing singleton*.
+%   Allows user to choose scene, then instance category. The user then can use GUI 
+%     buttons or keyboard shortcuts to edit or remove the boxes for the chosen 
+%     instance in the chosen scene.
 %
-%      BOX_EDITOR('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in BOX_EDITOR.M with the given input arguments.
+%   Loads all rgb images for the given scene once the scene is selected, so for  
+%     best performance, edit all instances in one scene before moving to the next scene
 %
-%      BOX_EDITOR('Property','Value',...) creates a new BOX_EDITOR or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before box_editor_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to box_editor_OpeningFcn via varargin.
+%   Bounding boxes are assumed to be in [xmin ymin xmax ymax] format
 %
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
+%   NOTE:  there currently is a bug that does not allow the user to change which scene
+%          they are working on properly. So pick one scene, edit boxes, and then restart
+%          the GUI for the next scene.
 
-% Edit the above text to modify the response to help box_editor
+%% TODO  - load jpg images
+%        - change scenes properly
+%        - make sure loading of raw labeles gets most recent file, 
+%             even ones after the GUI is started
+%        - more memory for undo
 
-% Last Modified by GUIDE v2.5 03-Sep-2016 14:05:56
+
+%% Last Modified by GUIDE v2.5 03-Sep-2016 14:05:56
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -47,11 +46,13 @@ end
 % --- Executes just before box_editor is made visible.
 function box_editor_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
+%
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to box_editor (see VARARGIN)
 
+%get all the scene names and populate the drop down menu
 init;
 d = dir(ROHIT_BASE_PATH);
 d = d(3:end);
@@ -72,6 +73,7 @@ guidata(hObject, handles);
 % --- Outputs from this function are returned to the command line.
 function varargout = box_editor_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
+%
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -82,28 +84,37 @@ varargout{1} = handles.output;
 
 % --- Executes on button press in delete_button.
 function delete_button_Callback(hObject, eventdata, handles)
+% remove current box/image from label list
+%
 % hObject    handle to delete_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+%get the current list of image names and boxes, 
+%as well as the index of the current image
 image_names = handles.image_names;
 bboxes = handles.bboxes;
 cur_image_index = handles.cur_image_index;
 
+%save the box/image we are about to delete incase user wants to undo
 handles.last_deleted_name = image_names{cur_image_index};
 handles.last_deleted_boxes = bboxes{cur_image_index};
 
+%remove the box/image from the lists
 image_names(cur_image_index) = [];
 bboxes(cur_image_index) = [];
 
+%make sure index does not go out of bounds
 if(cur_image_index > length(image_names))
   cur_image_index = 1;
 end
 
+%set the new lists 
 handles.image_names = image_names;
 handles.bboxes = bboxes;
 handles.cur_image_index = cur_image_index;
 
+%update everything
 guidata(hObject, handles);
 draw_current_image_and_box(hObject, eventdata,handles)
 
@@ -111,11 +122,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in corner_up_button.
 function corner_up_button_Callback(hObject, eventdata, handles)
+% moves bottom edge of box up by change resolution 
+%
 % hObject    handle to corner_up_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the current box 
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the bottom edge(ymax == bbox(4)) up, 
+%but make sure the box still has height of 1
 if(bbox(4) - handles.box_change_resolution >= bbox(2))
   bbox(4) = bbox(4) - handles.box_change_resolution;
 else
@@ -130,11 +147,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in corner_down_button.
 function corner_down_button_Callback(hObject, eventdata, handles)
+% moves the bottom edge of the box down by the change resolution
+% but not out of the image
+%
 % hObject    handle to corner_down_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the edge, keeping it inbounds
 if(bbox(4) + handles.box_change_resolution < handles.image_height)
   bbox(4) = bbox(4) + handles.box_change_resolution;
 else
@@ -148,11 +171,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in corner_right_button.
 function corner_right_button_Callback(hObject, eventdata, handles)
+% moves the right edge of the box right by the change resolution
+% but not out of the image
+%
 % hObject    handle to corner_right_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box 
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the edge
 if(bbox(3) + handles.box_change_resolution < handles.image_width)
   bbox(3) = bbox(3) + handles.box_change_resolution;
 else
@@ -167,11 +196,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in corner_left_button.
 function corner_left_button_Callback(hObject, eventdata, handles)
+% moves the right edge of the box left by the change resolution
+% but ensures a minimum width of 1 for the box 
+%
 % hObject    handle to corner_left_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the edge
 if(bbox(3) - handles.box_change_resolution >= bbox(1))
   bbox(3) = bbox(3) - handles.box_change_resolution;
 else
@@ -188,11 +223,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in box_up_button.
 function box_up_button_Callback(hObject, eventdata, handles)
+% moves the entire box up by the change resolution,
+% but keeps the box in the image
+%
 % hObject    handle to box_up_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the box
 if(bbox(2) - handles.box_change_resolution >= 1)
   bbox(2) = bbox(2) - handles.box_change_resolution;
   bbox(4) = bbox(4) - handles.box_change_resolution;
@@ -209,11 +250,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in box_down_button.
 function box_down_button_Callback(hObject, eventdata, handles)
+% moves the entire box down by the change resolution,
+% but keeps the box in the image
+%
 % hObject    handle to box_down_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the box
 if(bbox(4) + handles.box_change_resolution < handles.image_height)
   bbox(2) = bbox(2) + handles.box_change_resolution;
   bbox(4) = bbox(4) + handles.box_change_resolution;
@@ -231,11 +278,16 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in box_right_button.
 function box_right_button_Callback(hObject, eventdata, handles)
+% moves the entire box right by the change resolution,
+% but keeps the box in the image
+%
 % hObject    handle to box_right_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
 
+%move the box
 if(bbox(3) + handles.box_change_resolution < handles.image_width)
   bbox(1) = bbox(1) + handles.box_change_resolution;
   bbox(3) = bbox(3) + handles.box_change_resolution;
@@ -251,10 +303,17 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in box_left_button.
 function box_left_button_Callback(hObject, eventdata, handles)
+% moves the entire box left by the change resolution,
+% but keeps the box in the image
+%
 % hObject    handle to box_left_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+%get the box
 bbox = handles.bboxes{handles.cur_image_index};
+
+%move the box
 if(bbox(1) - handles.box_change_resolution > 0)
   bbox(1) = bbox(1) - handles.box_change_resolution;
   bbox(3) = bbox(3) - handles.box_change_resolution;
@@ -271,6 +330,8 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in prev_button.
 function prev_button_Callback(hObject, eventdata, handles)
+% move to the previous image (wraps around)
+%
 % hObject    handle to prev_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -284,6 +345,8 @@ draw_current_image_and_box(hObject, eventdata,handles)
 
 % --- Executes on button press in next_button.
 function next_button_Callback(hObject, eventdata, handles)
+% move to the next image (wraps around)
+%
 % hObject    handle to next_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -293,12 +356,14 @@ if(handles.cur_image_index > length(handles.image_names))
 end
 
 guidata(hObject, handles);
-
 draw_current_image_and_box(hObject, eventdata,handles)
 
 
 % --- Executes on selection change in scene_pop_up_menu.
 function scene_pop_up_menu_Callback(hObject, eventdata, handles)
+% respond to user choice of scene in popup menu
+% loads all rgb images for the scene and populates instance menu
+%
 % hObject    handle to scene_pop_up_menu (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -306,18 +371,24 @@ function scene_pop_up_menu_Callback(hObject, eventdata, handles)
 % Hints: contents = cellstr(get(hObject,'String')) returns scene_pop_up_menu contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from scene_pop_up_menu
 init;
+
+%get the string values of the selected item from the menu and save it
 contents = cellstr(get(hObject,'String'));
 handles.selected_scene =  contents{get(hObject,'Value')};
+
+%get all the instance names and populate the menu
 instance_labels = dir(fullfile(ROHIT_META_BASE_PATH, handles.selected_scene, ...
                       'labels','raw_labels', 'bounding_boxes_by_instance', '*.mat'));
 instance_labels = {instance_labels.name};
-
 handles.label_pop_up_menu.String = cat(2,{'Pick a label'},instance_labels);
 
-all_image_names = get_names_of_X_for_scene(handles.selected_scene, 'rgb_images');
 
+%get all the rgb image names for this scene
+all_image_names = get_names_of_X_for_scene(handles.selected_scene, 'rgb_images');
 images = cell(1,length(all_image_names));
 
+%load all the images
+%display progress to user
 tt = text(.3,.5,['Loaded Image ' num2str(0) '/' num2str(length(all_image_names))]);
 
 for il=1:length(all_image_names)
@@ -325,12 +396,12 @@ for il=1:length(all_image_names)
   img = imread(fullfile(ROHIT_BASE_PATH,handles.selected_scene, 'rgb', ...
                       strcat(cur_name(1:10), '.png')));
   images{il} = img;
-  
   delete(tt);
   tt = text(.3,.5,['Loaded Image ' num2str(il) '/' num2str(length(all_image_names))]);
   drawnow
 end
 
+%save all the loaded images into a map
 handles.image_map = containers.Map(all_image_names, images);
 handles.image_width = size(img,2);
 handles.image_height = size(img,1);
@@ -353,6 +424,8 @@ end
 
 % --- Executes on selection change in label_pop_up_menu.
 function label_pop_up_menu_Callback(hObject, eventdata, handles)
+% load the labels(boxes) for the selected instance in the selected scene
+%
 % hObject    handle to label_pop_up_menu (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -360,15 +433,20 @@ function label_pop_up_menu_Callback(hObject, eventdata, handles)
 % Hints: contents = cellstr(get(hObject,'String')) returns label_pop_up_menu contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from label_pop_up_menu
 init;
-%text(.3,.5,'Loading Images...');
 
+%get the string for the selected instance and save it
 contents = cellstr(get(hObject,'String'));
 handles.selected_instance =  contents{get(hObject,'Value')};
+
+%load the mat file with the boxes
 cur_instance_labels = load(fullfile(ROHIT_META_BASE_PATH, handles.selected_scene, ...
-                            'labels', 'raw_labels', 'bounding_boxes_by_instance', handles.selected_instance));
+                            'labels', 'raw_labels', 'bounding_boxes_by_instance', ...
+                              handles.selected_instance));
+
+%save the current list of image names and boxes for this instance
 handles.image_names = cur_instance_labels.image_names;
 handles.bboxes = cur_instance_labels.boxes;
-
+%start from the first image
 handles.cur_image_index = 1;
 
 draw_current_image_and_box(hObject, eventdata,handles)
@@ -393,16 +471,20 @@ end
 
 
 function draw_current_image_and_box(hObject, eventdata,handles)
+%draws the current image and box on the GUI
 
+%get the current image name, and image matrix
 cur_image_name = handles.image_names{handles.cur_image_index};
-
 img = handles.image_map(cur_image_name);
+
+%plot the image, then draw the box on top
 imshow(img);
 hold on;
 bbox = handles.bboxes{handles.cur_image_index};
 rectangle('Position',[bbox(1) bbox(2) (bbox(3)-bbox(1)) (bbox(4)-bbox(2))], ... 
                      'LineWidth',2, 'EdgeColor','r');
-                   
+                  
+%indicate what box this is (i.e. box 3 of 100) 
 box_string = [num2str(handles.cur_image_index) '/' ...
                 num2str(length(handles.image_names))];
 handles.box_counter.String = box_string;
@@ -415,6 +497,8 @@ guidata(hObject, handles);
 
 % --- Executes on key press with focus on figure1 or any of its controls.
 function figure1_WindowKeyPressFcn(hObject, eventdata, handles)
+% Make keyboard shortcuts. Map keypresses to button callbacks
+%
 % hObject    handle to figure1 (see GCBO)
 % eventdata  structure with the following fields (see MATLAB.UI.FIGURE)
 %	Key: name of the key that was pressed, in lower case
@@ -448,11 +532,20 @@ switch keyPressed
     corner_right_button_Callback(hObject, eventdata, handles)
   case 'x'
     delete_button_Callback(hObject, eventdata, handles)
+  case 'u'
+    undo_button_Callback(hObject, eventdata, handles)
+  case 'v'
+    res_down_button_Callback(hObject, eventdata, handles)
+  case 'b'
+    res_up_Callback(hObject, eventdata, handles)
 end
 
 
 % --- Executes on button press in save_button.
 function save_button_Callback(hObject, eventdata, handles)
+% Saves the boxes for the current instance
+% does not overwrite loaded boxes, but saves boxes in new folder as verified
+%
 % hObject    handle to save_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -466,6 +559,9 @@ save(fullfile(ROHIT_META_BASE_PATH, handles.selected_scene, ...
 
 % --- Executes on button press in res_up_button.
 function res_up_button_Callback(hObject, eventdata, handles)
+% increases the change resoluition 
+% (how many pixels the box/edge will move for a single edit command)
+%
 % hObject    handle to res_up_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -476,6 +572,9 @@ guidata(hObject, handles);
 
 % --- Executes on button press in res_down_button.
 function res_down_button_Callback(hObject, eventdata, handles)
+% decreases the change resoluition 
+% (how many pixels the box/edge will move for a single edit command)
+%
 % hObject    handle to res_down_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -487,6 +586,10 @@ guidata(hObject, handles);
 
 % --- Executes on button press in undo_button.
 function undo_button_Callback(hObject, eventdata, handles)
+% undoes the last deletion.
+% reinserts the last deleted box/image to the end of the lists
+% only does ONE. i.e can not get back the second to last deleted box 
+%
 % hObject    handle to undo_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
--- bounding_box_processing/classify_boxes.m
+++ bounding_box_processing/classify_boxes.m
@@ -1,13 +1,11 @@
-%saves a map from a label (instance) name, to names of all images that 'see'
-% any of the reconstructed points on the object
-
-%TODO -get rid of image structs map. Just use indexes. (Make it sorted?)
+% assigns a label to each bounding box indicating how difficult it may
+% for a detection system to duplicate
 
 
-%clearvars;
+%TODO
 
+clearvars;
 %initialize contants, paths and file names, etc. 
-
 init;
 
 
@@ -15,27 +13,18 @@ init;
 %% USER OPTIONS
 
 scene_name = 'Kitchen_Living_08_1'; %make this = 'all' to run all scenes
-group_name = 'all';
 model_number = '0';
 use_custom_scenes = 0;%whether or not to run for the scenes in the custom list
 custom_scenes_list = {'Bedroom_01_1', 'Kitchen_Living_01_1', 'Kitchen_Living_02_1', 'Kitchen_Living_03_1', 'Kitchen_Living_04_2', 'Kitchen_05_1', 'Kitchen_Living_06', 'Office_01_1'};%populate this 
 
 
-
-
+%which instances to use
 label_to_process = 'all'; %make 'all' for every label
 label_names = {label_to_process};
 
 
 
 debug =0;
-
-kinect_to_use = 1;
-
-%size of rgb image in pixels
-kImageWidth = 1920;
-kImageHeight = 1080;
-
 
 
 %% SET UP GLOBAL DATA STRUCTURES
@@ -77,7 +66,7 @@ for il=1:length(all_scenes)
 
 
   %% get camera info
-  fid_camera =  fopen(fullfile(meta_path,'reconstruction_results', group_name, ...
+  fid_camera =  fopen(fullfile(meta_path,'reconstruction_results', ...
                                 'colmap_results', model_number,'cameras.txt'));
 
  
@@ -110,7 +99,6 @@ for il=1:length(all_scenes)
   distortion(3) = str2double(line{11});
   distortion(4) = str2double(line{12});
 
-
   fclose(fid_camera);
 
 
@@ -142,61 +130,53 @@ for il=1:length(all_scenes)
 
 
 
-  %% MAIN LOOP  for each label find its bounding box in each image
+  %% MAIN LOOP for each intsance label, get all of its boxes and classify them 
 
-  %for each point cloud
+  %for each  instance label
   for jl=1:length(label_names)
     
     %get the name of the label
     cur_label_name = label_names{jl};
-    disp(cur_label_name);
+    disp(cur_label_name);%display progress
 
     %load the labeled point cloud for this label in this scene
     cur_pc = pcread(fullfile(meta_path,'labels', ...
                        'object_point_clouds', strcat(cur_label_name, '.ply')));
  
 
-                     
+    %load the boxes for this instance                
     try
-    cur_instance_boxes = load(fullfile(meta_path, 'labels', 'verified_labels', ...
+      cur_instance_boxes = load(fullfile(meta_path, 'labels', 'verified_labels', ...
                               'bounding_boxes_by_instance', strcat(cur_label_name, '.mat')));
     catch
+      %this instance hasn't been labeled yet, skip it
       continue;
     end
 
+    %get all the image names and boxes for the instance 
     image_names = cur_instance_boxes.image_names; 
     cur_instance_boxes = cur_instance_boxes.boxes;
 
-    %% for each image, determine if it 'sees' this object(point cloud) 
+    %% for each box,image classify the box 
     for kl = 1:length(image_names) 
-            
-      %just to see how much progress is being made
-      %if(mod(kl,50) == 0)
-      %  disp(cur_image_name);
-      %end
-      
-%       if(strcmp(cur_image_name,'0006460101.png'))
-%         breakp = 1;
-%       end
-      %% get the image name, position/direction info 
+      %get the image name, and the coressponding image struct     
       cur_image_name = image_names{kl};
       cur_image_struct = image_structs_map(cur_image_name);
-
-      %% get the posisiton and color of the object point cloud, and the global scene point cloud
-      cur_world_locs = cur_pc.Location;
-    
-    
-      %% now see what points the are in front of the camera
-      % point cloud locations,  in homogenous coordinates
-      cur_homog_points = [cur_world_locs ones(length(cur_world_locs), 1)]';
-
-
+      
       %get extrinsic parameters for this image 
       R = cur_image_struct.(ROTATION_MATRIX);
       t = cur_image_struct.(TRANSLATION_VECTOR);
 
+      %% get the posisiton of the object point cloud
+      cur_world_locs = cur_pc.Location;
+    
+    
+      %% now see what points the are in front of the camera
+      
+      % point cloud locations,  in homogenous coordinates
+      cur_homog_points = [cur_world_locs ones(length(cur_world_locs), 1)]';
 
-      %re-orient the point clouds to see if they are viewable by this camera
+      %re-orient the point cloud to see what points are viewable by this image
       P = [R t];
       oriented_points = P * cur_homog_points;
 
@@ -208,8 +188,7 @@ for il=1:length(all_scenes)
       cur_world_locs(bad_inds, :) = [];
 
 
-      %% project the point clouds onto the image plane
-
+      %% project the point cloud onto the image plane
       distorted_points = project_points_to_image(cur_world_locs, K, R, t, distortion);
 
       
@@ -225,28 +204,35 @@ for il=1:length(all_scenes)
       cur_world_locs(bad_inds,:) = [];
 
 
-      %% Check for occlusion using the captures(kinect) depth image.
-     
+      %% classify the box
+
+      %get dimensions of the box just found(without occlusion filtering) 
       minx = min(distorted_points(1,:));
       miny = min(distorted_points(2,:));
       maxx = max(distorted_points(1,:));
       maxy = max(distorted_points(2,:));
       box_area = (maxx - minx) * (maxy-miny);
 
- 
 
-
-
-
+      %get the true labeled box and its dimensions 
       labeled_box = cur_instance_boxes{kl};
       labeled_box_area = (labeled_box(3) - labeled_box(1)) * (labeled_box(4)-labeled_box(2));
       labeled_min_dim = min((labeled_box(3)-labeled_box(1)), (labeled_box(4)-labeled_box(2)));
       labeled_max_dim = max((labeled_box(3)-labeled_box(1)), (labeled_box(4)-labeled_box(2)));
-     
-      raw_area_hardness = 0;
-      ratio_area_hardness = 0;
-      num_points_hardness = 0;
+    
+      %three separate measures of hardness for a box. we will calculate each separately
+      %and then pick whichever indicates the highest 'hardness'. 
+      raw_area_hardness = 0;%is the box small?
+      ratio_area_hardness = 0;%is the true box much smaller than
+                              % box without occlusion filtering?
+      num_points_hardness = 0;%did only a few points from the point cloud 
+                              %project into this image? (object is cut off)
 
+
+
+
+      %raw area hardness (too small?)
+      %if one dimension is too small, it will be very hard to find
       if((labeled_max_dim > .1*kImageWidth)  && (labeled_min_dim > 40))
         raw_area_hardness = 0;
       elseif((labeled_max_dim > .05*kImageWidth)  && (labeled_min_dim > 30))
@@ -258,6 +244,9 @@ for il=1:length(all_scenes)
       end 
 
 
+      %ratio area hardness (kind of occlusion)
+      %if the true labeled box is only 50% the area of the box without
+      % occlusion filtering, very roughly 50% of the object is occluded
       area_ratio = labeled_box_area/box_area; 
       if(area_ratio > .8)
         ration_area_hardness = 0; 
@@ -270,6 +259,10 @@ for il=1:length(all_scenes)
       end
 
 
+      %num points hardness (cut off?)
+      %if only 50% of the point cloud projects into the image, 
+      %then very roughly %50 of the object is not in the image,
+      % maybe cut off on the boundary
       num_points_ratio = length(distorted_points)/ length(cur_pc.Location);
       if(num_points_ratio > .8)
         ration_area_hardness = 0; 
@@ -281,28 +274,26 @@ for il=1:length(all_scenes)
         ration_area_hardness = 3; 
       end 
 
-
+      %the final hardness is the max hardness of any of the three measures
       hardness = max([raw_area_hardness, ratio_area_hardness, num_points_hardness]);
- 
-      if(length(labeled_box) < 5)
-        cur_instance_boxes{kl} = [labeled_box hardness];
-      else
-        cur_instance_boxes{kl} = [labeled_box(1:4) hardness];
-      end
-    end
+
+      %add the hardness meseaure to the box 
+      cur_instance_boxes{kl} = [labeled_box(1:4) hardness];
+    end%for kl, each image name
+
+    %% save the newly classified boxes for this instance label
+
+    %create the struct for the boxes
     boxes = cur_instance_boxes;
     cur_instance_boxes = struct('image_names', cell(1), ...
                                 'boxes', cell(1));
     cur_instance_boxes.image_names = image_names;
     cur_instance_boxes.boxes = boxes;
-                             
-
+    %save 
     save(fullfile(meta_path, 'labels', 'verified_labels', ...
-                            'bounding_boxes_by_instance', strcat(cur_label_name, '.mat')), '-struct', 'cur_instance_boxes');
-
-
-    
-  end%for jl, each label struct
-
-end%for i, each scene_name
+                            'bounding_boxes_by_instance',...
+                             strcat(cur_label_name, '.mat')),...
+                               '-struct', 'cur_instance_boxes');
+  end%for jl, each instance label name 
+end%for il, each scene_name
 
